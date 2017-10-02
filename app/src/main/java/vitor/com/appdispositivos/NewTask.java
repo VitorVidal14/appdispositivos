e2a93e115d83d11baa1334a67be95691e017b065
Vitor Vidal <vitorvidal@unifei.edu.br>
2017-10-02 13:12:00 -0300

    2.0.0.2 Mudança na cor do Fab; Criada a opção para voltar a tela inicial a partir da tela de criar uma nova nota; O espaço ocupado pela caixa "Mensagem" foi aumentado;

--- app/src/main/java/vitor/com/appdispositivos/NewTask.java
+++ app/src/main/java/vitor/com/appdispositivos/NewTask.java
@@ -1,7 +1,10 @@
 package vitor.com.appdispositivos;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class NewTask extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class NewTask extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_task);
+        setupActionBar();
+    }
+
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            startActivity(new Intent(NewTask.this, MainActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
