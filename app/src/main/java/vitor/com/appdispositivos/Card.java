956985c2d72b583bec31426d4a3bd639af4a4111
saviollage <saviohenrique.lage@unifei.edu.br>
2017-10-16 10:29:19 -0200

    Version 2.0.0.5 -> Gabriel/João/Sávio Fizemos a linkagem  do activity_new_task.xml ( parte de criacao de notas) o botao concluir ja esta em funcionamento Falta criar  a parte de favoritos e remocação

--- app/src/main/java/vitor/com/appdispositivos/Card.java
+++ app/src/main/java/vitor/com/appdispositivos/Card.java
@@ -1,17 +1,20 @@
 package vitor.com.appdispositivos;
 
+import android.icu.util.Calendar;
+
 /**
  * Created by saviolage on 16/10/17.
  */
 
-public class Card {
+public class Card{
 
     //VALORES BASE
     private String title;
-    private String text; 
-    private boolean fav;
+    private String text;
+    private boolean fav; // FALSE = NO PRIORITY (DEFAULT)
 
     //DETALHES DO DIA DE CRIACAO DO CARD
+    private Calendar cal;
     private int dayOfCreation;
     private int monthOfCreation;
     private int yearOfCreation;
@@ -19,7 +22,26 @@ public class Card {
     private int minuteOfCreation;
     private int secondOfCreation;
 
+    //void image; <- Imagem tirada e anexada a nova tarefa, opcional.
+
     private double milisecond; //PARA COMPARAR QUAL CARD MAIS RECENTE
+
+
+    public Card (){ // bob, the builder
+        this.title = "";
+        this.text = "";
+        this.fav = false;
+        this.cal = Calendar.getInstance(); // Requires minimum API level 24
+
+        this.dayOfCreation = cal.DAY_OF_MONTH;
+        this.monthOfCreation=cal.MONTH;
+        this.yearOfCreation=cal.YEAR;
+        this.hourOfCreation=cal.HOUR;
+        this.minuteOfCreation=cal.MINUTE;
+        this.secondOfCreation=cal.SECOND;
+
+
+    }
 
 
     public String getTitle() {
--- app/src/main/java/vitor/com/appdispositivos/NewTask.java
+++ app/src/main/java/vitor/com/appdispositivos/NewTask.java
@@ -1,18 +1,36 @@
 package vitor.com.appdispositivos;
 
 import android.content.Intent;
+import android.icu.util.Calendar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class NewTask extends AppCompatActivity {
+
+    private View Text;
+    ArrayList<Card> listCard;
+
+
+    public NewTask (){
+        listCard = new ArrayList<Card>();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_task);
         setupActionBar();
+
     }
 
     private void setupActionBar() {
@@ -31,4 +49,25 @@ public class NewTask extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public void acao (View view){
+
+        Card card = new Card();
+
+        //Passando title pro card
+        EditText get_text_titulo = view.findViewById(R.id.text_titulo);
+        card.setTitle(get_text_titulo.toString());
+
+        //Passando description pro card
+        EditText get_description = view.findViewById(R.id.text_description);
+        card.setText(get_description.toString());
+
+
+        listCard.add(card); //NO FINAL DE TUDO ADICIONA O CARD NO VETOR
+
+    }
+
+
+
 }
+
