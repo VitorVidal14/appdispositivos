84969093e1523523febbf68443e1f9463e0c9951
VitorVidal14 <vitorvidal@unifei.edu.br>
2017-10-23 10:59:42 -0200

    2.0.14 Uma nova activity foi criada para mostrar os cards na tela principal

--- app/src/main/java/vitor/com/appdispositivos/MainActivity.java
+++ app/src/main/java/vitor/com/appdispositivos/MainActivity.java
@@ -3,7 +3,6 @@ package vitor.com.appdispositivos;
 import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
