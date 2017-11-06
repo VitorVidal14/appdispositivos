331e67336b4aadd67d0b48290e142d7ac7a239fd
pedrobispo <pedrobispo@unifei.edu.br>
2017-11-06 08:56:28 -0200

    2.0.19 - // adicionado info Adicionado funcoes ao settings

--- app/src/main/java/vitor/com/appdispositivos/ArmazenamentoInterno.java
+++ app/src/main/java/vitor/com/appdispositivos/ArmazenamentoInterno.java
@@ -7,8 +7,8 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
-import javax.naming.Context;
 
+/** Desgraca */
 /**
  * Created by aluno on 30/10/17.
  */
